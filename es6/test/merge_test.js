5101e2f6e9da6a8e1eebe07df2a762c1171c2376
Ramesh VK <ramesh@do_not_email_me.com>
2019-11-23 19:51:33 -0800

    merge: Add replace() and replacePath()

--- es6/test/merge_test.js
+++ es6/test/merge_test.js
@@ -6,25 +6,27 @@ import { expect } from "./expect.js";
 import { wrap, merge } from "../main.js";
 
 describe("merge", () => {
-  it("combines duplicate keys", () => {
-    let s1 = wrap({hello: "world", ok: "computer"});
-    let s2 = wrap({hello: "boo", boo: "hoo"});
+  it("replaces path", () => {
+    let s1 = wrap({ hello: "world", ok: "computer" });
+    let s2 = wrap({ hello: "boo", boo: "hoo" });
     let s = merge([s1, s2]);
+    s.replacePath(["hello"], "goodbye");
+    s = s.latest();
     expect(JSON.parse(JSON.stringify(s))).to.deep.equal({
-      hello: "boo",
+      hello: "goodbye",
       boo: "hoo",
       ok: "computer"
     });
   });
 
   it("iterates through all keys", () => {
-    let s1 = wrap({hello: "world"});
-    let s2 = wrap({hello: "boo", boo: "hoo"});
+    let s1 = wrap({ hello: "world" });
+    let s2 = wrap({ hello: "boo", boo: "hoo" });
     let s = merge([s1, s2]);
     let keys = {};
     s.forEachKey(key => {
       keys[key] = (keys[key] || 0) + 1;
     });
-    expect(keys).to.deep.equal({hello: 1, boo: 1});
+    expect(keys).to.deep.equal({ hello: 1, boo: 1 });
   });
 });
